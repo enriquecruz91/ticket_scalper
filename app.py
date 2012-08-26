87a9d35b71d6164d1c467d57936b44d6c9487622
Enrique Cruz <eac2192@columbia.edu>
2012-08-25 22:43:18 -0400

    more

--- app.py
+++ app.py
@@ -52,5 +52,5 @@ def buy():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='127.0.0.1', port=port)
     #app.run()
