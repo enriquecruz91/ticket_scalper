e093a6f46a51de76f0e78b13916c01674802f7d3
Enrique Cruz <eac2192@columbia.edu>
2012-08-25 22:16:01 -0400

    switeched to deploy env

--- app.py
+++ app.py
@@ -51,6 +51,6 @@ def buy():
     return render_template('buy.html')
 
 if __name__ == "__main__":
-    #port = int(os.environ.get("PORT", 5000))
-    #app.run(host='0.0.0.0', port=port)
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+    #app.run()
